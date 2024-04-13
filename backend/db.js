d2f82ce5993dca6be703df51de6fa566f61b0529
MuhammadBilalKhan267 <bilalkhanawm0@gmail.com>
2024-04-13 19:42:25 +0500

    added password hashing

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongooseURI = "mongodb://localhost:27017";
+const mongooseURI = "mongodb://localhost:27017/cloudbook";
 
 const connectToDB = ()=>{
     mongoose.connect(mongooseURI).then(()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,63 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "bilalisagoodboi";
 
 
+//Create a new user
+router.post('/createUser', [
+    body('email', 'Invalid Email').isEmail(),
+    body('name', 'Name must be atleast 3 characters').isLength({min: 3}),
+    body('password', 'Password must be at least 8 characters').isLength({min: 8})
+], async (req, res)=>{
+    const result = validationResult(req);
+    
+    //Return error array if invalid data is sent
+    if (!result.isEmpty()) {
+        return res.send({ errors: result.array() });
+    }
+    
+    try{
+        //Check if email is available
+        let user =  await User.findOne({email: req.body.email});
+        if (user){
+            return res.status(400).json({error: "Email already in use!"});
+        }
 
-router.post('/', (req, res)=>{
-    const user = User(req.body);
-    user.save();
-    console.log(req.body);
-    res.send("hello");
-})
+        const salt = await bcrypt.genSalt();
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+        user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email
+        })
+
+        const data = {
+            id: user.data
+        };
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        
+
+        res.send({authToken});
+        // .catch(error => {
+        //     console.log(error);
+        //     res.json({error : "Email already in use!"});
+        // })
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).json("Unexpected error!");
+    }
+});
+
 router.get('/', (req, res)=>{
     res.redirect('http://localhost:3000');
-})
+});
 
 module.exports = router
